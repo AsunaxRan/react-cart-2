e987bbff5b2042b311ba784d19911de80c8b6eed
AsunaxRan <chimsedinangidol@gmail.com>
2020-11-16 23:23:23 +0700

    Modify MockDb

--- tools/createMockDb.js
+++ tools/createMockDb.js
@@ -5,30 +5,62 @@ const path = require("path");
 
 const faker = require("faker");
 
-const ranColor = ["red", "orange", "yellow", "green", "blue"];
-const ranSize = ["XS", "XS/S", "S", "S/M", "M"];
-const ranBrand = ["Louis Vuitton", "Lacoste", "Gucci", "Chanel", "Balenciaga"];
+const randColor = ["red", "orange", "yellow", "green", "blue"];
+const randSize = ["XS", "S", "M", "L", "XL", "XXL"];
+const randBrand = ["Louis Vuitton", "Lacoste", "Gucci", "Chanel", "Balenciaga"];
+
+function slugify(text) {
+  return text
+    .toString()
+    .trim()
+    .toLowerCase()
+    .replace(/\s+/g, "-")
+    .replace(/[^\w-]+/g, "")
+    .replace(/--+/g, "-")
+    .replace(/^-+/, "")
+    .replace(/-+$/, "");
+}
 
 function generateEmployees() {
   const products = [];
   for (let id = 0; id < 100; id++) {
-    let name = faker.name.title();
-    let image = faker.image.fashion();
+    let name = faker.commerce.productName();
+    let images = [...Array(_.random(1, 4)).keys()].map(() =>
+      faker.image.fashion()
+    );
     let price = _.random(20, 500);
-    let color = _.sample(ranColor);
-    let description = faker.lorem.words(50);
+    let onSale = _.random(0, 1) ? true : false;
+    let promotionPrice = onSale
+      ? price - (price / 100) * _.random(0, 20)
+      : price;
+    let colors = [...Array(_.random(1, randColor.length)).keys()].map(
+      (item, key) => randColor[key]
+    );
+    let material = faker.commerce.productMaterial();
+    let sizes = [...Array(_.random(1, randSize.length)).keys()].map(
+      (item, key) => randSize[key]
+    );
+    let description = faker.lorem.paragraph();
+    let url = slugify(name);
 
     products.push({
       id: id,
       name: name,
       description: description,
-      price: price,
-      color: color,
-      image: image,
-      size: _.sample(ranSize),
-      brand: _.sample(ranBrand),
+      price: {
+        current: price,
+        promotion: promotionPrice,
+        onSale: onSale,
+      },
+      colors: colors,
+      material: material,
+      images: images,
+      sizes: sizes,
+      brand: _.sample(randBrand),
+      url: url,
     });
   }
+
   return { products: products };
 }
 
